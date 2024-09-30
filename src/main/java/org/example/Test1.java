b4fcdc13d1d99284bb6b03af427507c69a4e5a85
xuankim <kimxuan10a24@gmail.com>
2024-09-30 22:56:20 +0700

    add run1 function

--- src/main/java/org/example/Test1.java
+++ src/main/java/org/example/Test1.java
@@ -1,10 +1,14 @@
 package org.example;
 
+
 import org.testng.annotations.Test;
 
 public class Test1 {
     @Test
     public void run (){
         System.out.println("hi");
+    }
+    public  void run1(){
+        System.out.println("Hello");
     }
 }
